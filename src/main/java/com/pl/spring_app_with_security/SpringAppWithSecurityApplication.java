9ab02e338510b3e91837568937fdcb740b629607
Sylwia Duda <sylwia.agnieszka.duda@gmail.com>
2019-05-25 22:25:39 +0200

    first commit

---
+++ src/main/java/com/pl/spring_app_with_security/SpringAppWithSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.pl.spring_app_with_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppWithSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAppWithSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pl/spring_app_with_security/SpringAppWithSecurityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pl.spring_app_with_security;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringAppWithSecurityApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
